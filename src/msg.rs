fd7a12581c22cadcad882ac72431603b671fe540
motii <108280115+motii8128@users.noreply.github.com>
2024-10-07 08:27:11 +0900

    Add files via upload

--- src/msg.rs
+++ src/msg.rs
@@ -8,3 +8,27 @@ pub struct Float32{
 }
 
 impl FLSMsg for Float32 {}
+#[derive(Serialize, Deserialize, Clone)]
+pub struct Int32{
+    pub  data:i32,
+}
+
+impl FLSMsg for Int32 {}
+#[derive(Serialize, Deserialize, Clone)]
+pub struct Float64{
+    pub  data:f64,
+}
+
+impl FLSMsg for Float64 {}
+#[derive(Serialize, Deserialize, Clone)]
+pub struct Int64{
+    pub  data:i64,
+}
+
+impl FLSMsg for Int64 {}
+#[derive(Serialize, Deserialize, Clone)]
+pub struct Bool{
+    pub  data:bool,
+}
+
+impl FLSMsg for Bool {}
